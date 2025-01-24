d16d3c82cfbf2414824443b16016a7db57f078ae
Frysix <0frysix0@gmail.com>
2025-01-24 02:09:14 -0500

    fck me

--- ezCMD.bat
+++ ezCMD.bat
@@ -5,6 +5,6 @@ for /f "usebackq delims=" %%a in ("%~dp0\Status.txt") do (set "Status=%%a")
 if "!Status!"=="False" (goto end)
 powershell -executionpolicy bypass -command "if (test-path -path """%~dp0\Status.txt""") {remove-item -path """%~dp0\Status.txt""" -recurse -force}"
 powershell -executionpolicy bypass -command "if (-not (test-path -path """%~dp0\ezCMD.ps1""")) {function Get-InternetStatus {if (test-connection 'google.com' -count 1 -quiet) {return $true} else {return $false}} while (-not (Get-InternetStatus)) {Write-Host 'Please Connect to internet to install'} Invoke-Webrequest -uri 'https://raw.githubusercontent.com/Frysix/ezCMD/refs/heads/main/ezCMD.ps1' -outfile """%~dp0\ezCMD.ps1"""}"
-start powershell -executionpolicy bypass -windowstyle hidden -file "%~dp0\ezCMD.ps1" -verb runas
+start powershell -executionpolicy bypass -file "%~dp0\ezCMD.ps1" -verb runas
 :end
 exit
